0c77e166dd5e71d01233e7b0e30839a09fee6124
firdiseptiady <firdis998@gmail.com>
2018-04-06 21:20:00 +0800

    buat grtter and setter

--- src/praktek3/Elektronik.java
+++ src/praktek3/Elektronik.java
@@ -11,4 +11,30 @@ public class Elektronik {
     void cetakInfo(){
         System.out.println("Merk \t: "+merk+"\n"+"Jenis \t: "+jenis+"\n"+"Harga \t:"+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
