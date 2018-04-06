c19301623bb3b0ca9d6b692a200c57afe67c7bb7
firdiseptiady <firdis998@gmail.com>
2018-04-06 21:27:32 +0800

    menggunakan method set

--- src/praktek3/elektronikAksi.java
+++ src/praktek3/elektronikAksi.java
@@ -2,10 +2,12 @@ package praktek3;
 public class elektronikAksi {
     public static void main(String[] args) {
         Elektronik e = new Elektronik();
-        e.merk="politron";
-        e.jenis="televisi";
-        e.harga=1200000;
-        
+       
+       e.setMerk("Sharf");
+       e.setJenis("ac");
+       e.setHarga(3000000);
+       
+       
         e.cetakInfo();
     }
 }
