d97f514cfa6ae8b83acf3b8f87fe87646a8d076a
firdiseptiady <firdis998@gmail.com>
2018-04-06 21:14:52 +0800

    instance, buat atribut dan panggil method

--- src/praktek3/elektronikAksi.java
+++ src/praktek3/elektronikAksi.java
@@ -1,6 +1,11 @@
 package praktek3;
 public class elektronikAksi {
     public static void main(String[] args) {
+        Elektronik e = new Elektronik();
+        e.merk="politron";
+        e.jenis="televisi";
+        e.harga=1200000;
         
+        e.cetakInfo();
     }
 }
