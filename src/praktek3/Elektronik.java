8c57a33ecdaa60e338f2b7c84c873d29c768b936
firdiseptiady <firdis998@gmail.com>
2018-04-06 21:01:50 +0800

    buat method cetakinfo()

--- src/praktek3/Elektronik.java
+++ src/praktek3/Elektronik.java
@@ -1,7 +1,14 @@
 package praktek3;
 
+import static java.time.Clock.system;
+
 public class Elektronik {
     String merk;
     String jenis;
     double harga;
+    
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+merk+"\n"+"Jenis \t: "+jenis+"\n"+"Harga \t:"+harga);
+    }
 }
