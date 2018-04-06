f4a18d447036197fca64f871ee7666d1ab3ae219
firdiseptiady <firdis998@gmail.com>
2018-04-06 21:45:56 +0800

    atribut dan method

--- src/praktek3/kulkas.java
+++ src/praktek3/kulkas.java
@@ -1,5 +1,10 @@
 package praktek3;
 
 public class kulkas extends Elektronik{
+    private String jeniskulkas;
     
+    void throttle(){
+        System.out.println("druttttttt...............");
+        
+    }
 }
